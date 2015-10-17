9177ac0c2e2ab831dae1375bfe04aa8ef16d3f58
tdhancii <t.dhancii@gmail.com>
2015-10-17 16:06:32 +1300

    Mockito Test Cases Initial Commit

--- src/test/java/com/currconv/controller/ConversionControllerTest.java
+++ src/test/java/com/currconv/controller/ConversionControllerTest.java
@@ -1,187 +1,149 @@
 package com.currconv.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import org.junit.Before;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.hamcrest.core.AnyOf;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.currconv.bean.ConversionBean;
 import com.currconv.delegate.CurrencyConversionDelegate;
 import com.currconv.delegate.MasterDataDelegate;
 import com.currconv.delegate.UserDelegate;
 import com.currconv.dto.UserActivityDTO;
 import com.currconv.dto.UserDTO;
-import com.currconv.spring.BaseConfiguration;
-import com.currconv.spring.WebConfiguration;
-import com.currconv.test.util.ApplicationObjectBuilder;
+import com.currconv.entity.UserActivity;
+import com.currconv.exception.RemoteException;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = { WebConfiguration.class,
-	BaseConfiguration.class }, loader = AnnotationConfigWebContextLoader.class)
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.any;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class ConversionControllerTest {
 
-    @Autowired
-    protected WebApplicationContext webApplicationContext;
-
-    @Mock
-    private CurrencyConversionDelegate mockConversionDelegate;
-
-    @Mock
-    private UserDelegate mockUserDelegate;
-
-    @Mock
-    private MasterDataDelegate mockMasterDataDelegate;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-	// MockitoAnnotations.initMocks(this);
-	mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
-
-    @Test
-    public void loadConvertCurrencyValidNewUserTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-
-	mockMvc.perform((get("/convert")).session(session)).andExpect(status().isOk())
-		.andExpect(model().attribute("userActivityList", new ArrayList<UserActivityDTO>())).andExpect(
-			request().sessionAttribute("userName", userDTO.getFirstName() + " " + userDTO.getLastName()));
-    }
-
-    @Test
-    public void loadConvertCurrencyValidExistingUserTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	List<UserActivityDTO> userActivityList = new ArrayList<>();
-	userDTO.setUserActivityList(userActivityList);
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-	mockMvc.perform((get("/convert")).session(session)).andExpect(status().isOk())
-		.andExpect(model().attribute("userActivityList", userActivityList)).andExpect(
-			request().sessionAttribute("userName", userDTO.getFirstName() + " " + userDTO.getLastName()));
-    }
-
-    @Test
-    public void loadConvertCurrencyInvalidUserTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	session.setAttribute("user", null);
+	private static final String LOGIN_REDIRECT = "redirect:/login";
 	
-	mockMvc.perform(get("/convert").session(session))
-		.andExpect(request().sessionAttribute("user", null))	
-		.andExpect(model().attribute("userActivityList", new ArrayList<>()))
-		.andExpect(view().name("redirect:/login"))
-		.andExpect(request().sessionAttribute("loginMessage", "No user associated with the current session! Please re-login"))
-		.andExpect(status().isOk());
+		
+    @Mock
+    private CurrencyConversionDelegate conversionDelegate;
+    
+    @Mock
+    private UserDelegate userDelegate;
+
+    @Mock
+    private MasterDataDelegate masterDataDelegate;
+    
+    @Mock
+    private HttpServletRequest request;
+    
+    @Mock
+    private HttpSession session;
+    
+    
+    @Mock
+    private BindingResult bindingResult;
+    
+   
+    private UserDTO fixtureUserDTO() {
+    	
+    	UserDTO user = new UserDTO("emailID", "password", "firstName", "lastName", null, "addressLine1", "addressLine2", "street", "zip", "city", "country");
+    	
+    	return user;
     }
+    
+    @InjectMocks
+    private ConversionController conversionController = new ConversionController();
+    
     
     @Test
-    public void convertCurrencyValidNewUserTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	List<UserActivityDTO> userActivityList = new ArrayList<>();
-	userDTO.setUserActivityList(userActivityList);
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-	mockMvc.perform((get("/convert")).session(session)).andExpect(status().isOk())
-		.andExpect(model().attribute("userActivityList", userActivityList)).andExpect(
-			request().sessionAttribute("userName", userDTO.getFirstName() + " " + userDTO.getLastName()));
+    public void loadConvertCurrencyWihtoutUserShouRequestLogin() throws Exception {
+    	// Mock the Scenario    	
+    	when(request.getSession()).thenReturn(session);
+    	when(session.getAttribute("user")).thenReturn(null);
+    	ModelAndView model = new ModelAndView();
+    	ConversionBean conversionBean = new ConversionBean();
+    	    	
+    	// Invoker Controller
+    	ModelAndView modelAndView = conversionController.loadConvertCurrency(request, model, conversionBean);
+    	
+    	
+    	// Verify results
+    	assertEquals(modelAndView.getViewName(),LOGIN_REDIRECT);
     }
+
     
     @Test
-    public void convertCurrencyValidExistingUserTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	List<UserActivityDTO> userActivityList = new ArrayList<>();
-	userDTO.setUserActivityList(userActivityList);
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-	mockMvc.perform((get("/convert")).session(session)).andExpect(status().isOk())
-		.andExpect(model().attribute("userActivityList", userActivityList)).andExpect(
-			request().sessionAttribute("userName", userDTO.getFirstName() + " " + userDTO.getLastName()));
+    public void loadConvertCurrencyWithUserinSessionShouldLoadPage() throws Exception {
+    	// Mock the Scenario    	
+    	when(request.getSession()).thenReturn(session);
+    	UserDTO user = fixtureUserDTO();
+    	when(session.getAttribute("user")).thenReturn(user);
+    	ModelAndView model = new ModelAndView();
+    	ConversionBean conversionBean = new ConversionBean();
+    	    	
+    	// Invoker Controller
+    	ModelAndView modelAndView = conversionController.loadConvertCurrency(request, model, conversionBean);
+    	
+    	
+    	// Verify results
+    	assertTrue(modelAndView.getModel().keySet().contains("conversionBean"));   	
+    	assertTrue(modelAndView.getModel().keySet().contains("userActivityList"));    	
+    	
     }
+    
     
     @Test
-    public void convertCurrencyInvalidUserTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	List<UserActivityDTO> userActivityList = new ArrayList<>();
-	userDTO.setUserActivityList(userActivityList);
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-	mockMvc.perform((get("/convert")).session(session)).andExpect(status().isOk())
-		.andExpect(model().attribute("userActivityList", userActivityList)).andExpect(
-			request().sessionAttribute("userName", userDTO.getFirstName() + " " + userDTO.getLastName()));
+    public void convertCurrencyWithoutUserShouldRedirectToLogin() throws Exception {
+    	// Mock the scenario
+    	when(request.getSession()).thenReturn(session);
+    	when(session.getAttribute("user")).thenReturn(null);
+    	ModelAndView model = new ModelAndView();
+    	ConversionBean conversionBean = new ConversionBean();
+    	
+    	// Invoke Controller
+    	ModelAndView modelAndView = conversionController.convertCurrency(request, model, conversionBean, bindingResult);
+    	
+    	// Verify Results
+    	assertEquals(modelAndView.getViewName(),LOGIN_REDIRECT);    	    	
     }
     
-    @Test
-    public void convertCurrencyCurrencyConversionNotSupportedTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	List<UserActivityDTO> userActivityList = new ArrayList<>();
-	userDTO.setUserActivityList(userActivityList);
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-	mockMvc.perform((get("/convert")).session(session)).andExpect(status().isOk())
-		.andExpect(model().attribute("userActivityList", userActivityList)).andExpect(
-			request().sessionAttribute("userName", userDTO.getFirstName() + " " + userDTO.getLastName()));
+    @Test    
+    public void convertCurrencyretrieveCurrencyFailureShouldFail() throws Throwable {
+    	// Mock the Scenario
+    	when(request.getSession()).thenReturn(session);
+    	UserDTO user = fixtureUserDTO();
+    	when(session.getAttribute("user")).thenReturn(user);
+    	ModelAndView model = new ModelAndView();
+    	ConversionBean conversionBean = new ConversionBean();
+    	when(bindingResult.hasErrors()).thenReturn(false);
+    	when(conversionDelegate.retrieveCurrencyRates(anyString(), anyString(), any(BigDecimal.class), any(Date.class))).thenThrow(new RemoteException("Error"));
+    	
+    	// Invoke Controller
+    	conversionController.convertCurrency(request, model, conversionBean, bindingResult);
+    	
+    	// Verify Results
+    	verify(conversionDelegate,times(1)).retrieveCurrencyRates(anyString(), anyString(), any(BigDecimal.class), any(Date.class));
     }
-    
-    @Test
-    public void convertCurrencyRemoteExceptionTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	List<UserActivityDTO> userActivityList = new ArrayList<>();
-	userDTO.setUserActivityList(userActivityList);
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-	userActivityList.add(ApplicationObjectBuilder.buildUserActivityDTODummy());
-
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-	mockMvc.perform((get("/convert")).session(session)).andExpect(status().isOk())
-		.andExpect(model().attribute("userActivityList", userActivityList)).andExpect(
-			request().sessionAttribute("userName", userDTO.getFirstName() + " " + userDTO.getLastName()));
-    }
-    
-    
-    
 
 }
--- src/test/java/com/currconv/controller/LoginControllerTest.java
+++ src/test/java/com/currconv/controller/LoginControllerTest.java
@@ -1,117 +1,50 @@
 package com.currconv.controller;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.currconv.delegate.UserDelegate;
-import com.currconv.dto.UserDTO;
-import com.currconv.exception.AuthenticationFailureException;
-import com.currconv.exception.InvalidUserException;
-import com.currconv.spring.BaseConfiguration;
-import com.currconv.spring.WebConfiguration;
-import com.currconv.test.util.ApplicationObjectBuilder;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@SpringApplicationConfiguration(classes={WebConfiguration.class,BaseConfiguration.class})
-@EnableAutoConfiguration(exclude = { VelocityAutoConfiguration.class })
+
+@RunWith(MockitoJUnitRunner.class)
 public class LoginControllerTest {
 
     @Mock
     private UserDelegate userDelegateMock;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-	MockitoAnnotations.initMocks(this);
-	mockMvc = MockMvcBuilders.standaloneSetup(new LoginController()).build();
-    }
-
+    
+    
+    
     @Test
-    public void displayLoginNewUserSessionTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	session.setAttribute("user", null);
-	mockMvc.perform((get("/")).session(session).requestAttr("loginMessage", "")).andExpect(status().isOk())
-		.andExpect(view().name("login")).andExpect(forwardedUrl("login"));
+    public void displayLoginWithoutUserShouldReturnLogin() {
+    	
     }
-
+    
     @Test
-    public void displayLoginExistingUserSessionTest() throws Exception {
-
-	MockHttpSession session = new MockHttpSession();
-	session.setAttribute("user", ApplicationObjectBuilder.buildUserDTODummy());
-	mockMvc.perform((get("/")).session(session)).andExpect(status().isOk())
-		.andExpect(forwardedUrl("redirect:/convert"));
+    public void displayLoginWithUSerShouldGotoConvertCurrency() {
+    	
     }
-
+    
+    
     @Test
-    public void executeLoginInvalidUserExceptionTest() throws Exception {
-	when(userDelegateMock.authenticateUser("abc-1@xyz.com", "123456"))
-		.thenThrow(new InvalidUserException("User does not exist"));
-
-	mockMvc.perform(post("/login")).andExpect(forwardedUrl("login")).andExpect(status().isOk()).andExpect(
-		request().attribute("loginMessage", "User does not exist. Kindly register to access the system."));
+    public void executeLoginWithBindingErrorsShouldStayInLoginPage() {
+    	
     }
-
+    
     @Test
-    public void executeLoginAuthenticationFailureExceptionTest() throws Exception {
-	when(userDelegateMock.authenticateUser("abc-1@xyz.com", "123156"))
-		.thenThrow(new AuthenticationFailureException("Authentication Failure Occurred"));
-
-	mockMvc.perform(post("/login")).andExpect(forwardedUrl("login")).andExpect(status().isOk())
-		.andExpect(request().attribute("loginMessage", "Authentication Failure!"));
+    public void executeLoginInvalidUserShouldStayinLoginPage() {
+    	
     }
-
+    
     @Test
-    public void executeLoginSystemExceptionTest() throws Exception {
-	when(userDelegateMock.authenticateUser("abc-1@xyz.com", "123156"))
-		.thenThrow(new Exception("Authentication Failure Occurred"));
-
-	mockMvc.perform(post("/login")).andExpect(forwardedUrl("login")).andExpect(status().isOk())
-		.andExpect(request().attribute("loginMessage", "System Error occurred! Please visit us later!"));
+    public void executeLoginAuthenticalFailureShouldStayinLoginPage() {
+    	
     }
-
+    
     @Test
-    public void executeSuccessfulLoginTest() throws Exception {
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	when(userDelegateMock.authenticateUser("abc-1@xyz.com", "123456")).thenReturn(userDTO);
-	MockHttpSession session = new MockHttpSession();
-
-	mockMvc.perform(post("/login").session(session)).andExpect(status().isOk())
-		.andExpect(request().sessionAttribute("user", userDTO)).andExpect(forwardedUrl("redirect:/convert"));
+    public void executeLoginShouldPass(){
+    	
     }
 
-    @Test
-    public void executeSuccessfulLogoutTest() throws Exception {
-	UserDTO userDTO = ApplicationObjectBuilder.buildUserDTODummy();
-	MockHttpSession session = new MockHttpSession();
-	session.setAttribute("user", userDTO);
-	session.setAttribute("userNameData", userDTO.getFirstName());
-	mockMvc.perform(post("/logout").session(session)).andExpect(status().isOk())
-		.andExpect(request().sessionAttribute("user", null))
-		.andExpect(request().sessionAttribute("userNameData", null))
-		.andExpect(request().attribute("loginMessage", "You have successfully logged out"))
-		.andExpect(forwardedUrl("redirect:/convert"));
-    }
 }
